09b06726f7e433ebeb8ee9dea48c59ad6c03850c
asuletic <aleksandar.suletic@ctb.health>
2024-10-25 17:22:16 +0200

    added eslink and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "react/react-in-jsx-scope": "off",
+  },
+};
